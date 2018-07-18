bde650930f0108f918ff1a84c4b21798dce04a89
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-18 23:07:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_cart
 
-  def index
+  def cart
     session[:cart] ||= []
   end
 
   class Item
-    def create
+    def add_to_cart
       @item = Item.find(params[:id])
 
       current_cart = @item.id
